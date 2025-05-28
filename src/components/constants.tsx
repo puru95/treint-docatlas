205d25134084375cbb60c5c6f03b4e6b11e1ce9e
puru95 <pururathi31@gmail.com>
2025-05-28 17:52:11 +0530

    update

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -8,7 +8,7 @@ import { ReactComponent as Diseases } from '../assets/diseases_image.svg';
 export const TABS = [
     {
         id: "SYMPTOMS",
-        name: "Treatment Plan"
+        name: "Clinic Protocol"
     },
     {
         id: "MEDICINE",
